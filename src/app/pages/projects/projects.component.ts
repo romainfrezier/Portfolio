ca87530ce83bbb035bf59594e96c51f83a276446
romainfrezier <romainfrezier@gmail.com>
2023-06-21 09:17:31 +0200

    Removed secure link stuffs

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -2,7 +2,6 @@ import {Component} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {TranslateService} from "@ngx-translate/core";
 import {Project} from "../../models/project.model";
-import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-projects',
@@ -12,23 +11,10 @@ import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 export class ProjectsComponent {
 
   public projects!: Project[];
-  public demoLinks: (SafeResourceUrl | null)[];
 
-  constructor(private http: HttpClient, private translate: TranslateService, private sanitizer: DomSanitizer) {
-    this.demoLinks = [];
-    this.http.get<any>(`./assets/locales/${translate.currentLang}.json`).subscribe(data => {
+  constructor(private http: HttpClient, private translate: TranslateService) {
+    this.http.get<any>(`./assets/locales/${translate.currentLang}.json`).subscribe((data: any): void => {
       this.projects = data.portfolio.items;
-      for (let project of this.projects) {
-        if (project.demolink) {
-          this.demoLinks.push(this.sanitizer.bypassSecurityTrustResourceUrl(project.demolink));
-        } else {
-          this.demoLinks.push(null);
-        }
-      }
     });
-  }
-
-  public getSafeDemoLink(url: string): SafeResourceUrl {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
