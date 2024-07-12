36582ad8524ccb5306d715f7e77d50466c03fe15
Romain Frezier <romain.frezier@infomaniak.com>
2024-07-12 13:29:21 +0200

    fix(style): Fix style.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './components/header/header.component';
 import {LoaderComponent} from './components/loader/loader.component';
-import {MatIconModule} from '@angular/material/icon';
 import {TranslateModule} from '@ngx-translate/core';
 import {SafePipe} from '@pipes/safe.pipe';
 import {RouterModule} from '@angular/router';
@@ -11,7 +10,7 @@ import {ToastComponent} from "@shared/components/toast/toast.component";
 
 @NgModule({
   declarations: [HeaderComponent, LoaderComponent, SafePipe, FooterComponent, ToastComponent],
-  imports: [CommonModule, TranslateModule, MatIconModule, RouterModule],
+  imports: [CommonModule, TranslateModule, RouterModule],
   exports: [HeaderComponent, LoaderComponent, FooterComponent, ToastComponent, SafePipe],
 })
 export class SharedModule {}
