7f6bf6f8d99da9580a8a601b1ba9d815465e3c21
Romain Frezier <romain.frezier@infomaniak.com>
2024-07-17 15:37:30 +0200

    chore(tests): Add local storage mock.

---
+++ src/tests/mocks/local-storage.mock.ts
@@ -0,0 +1,19 @@
+export class MockLocalStorage {
+  private store: { [key: string]: string } = {};
+
+  getItem(key: string): string | null {
+    return this.store[key] || null;
+  }
+
+  setItem(key: string, value: string) {
+    this.store[key] = value;
+  }
+
+  removeItem(key: string) {
+    delete this.store[key];
+  }
+
+  clear() {
+    this.store = {};
+  }
+}
