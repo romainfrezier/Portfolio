70ab15bd980276cbf8551af26e7f0fb4307dc02a
romainfrezier <romainfrezier@gmail.com>
2024-09-17 06:08:22 +0200

    chore: Updating tests to correspond to the new work experience structure.

--- src/tests/fake.data.ts
+++ src/tests/fake.data.ts
@@ -4,6 +4,7 @@ import {WorkExperience} from "@models/work-experience.model";
 import {Achievement} from "@models/achievement.model";
 import {Skills} from "@models/skills.model";
 import {Link} from "@models/link.model";
+import {Position} from "@models/position.model";
 
 export const fakeSchoolProjects: SchoolProject[] = [
   {
@@ -28,13 +29,25 @@ export const fakePersonalProjects: PersonalProject[] = [
   }
 ];
 
+const fakePositions: Position[] = [
+  {
+    "description": 'Description of position 1',
+    "date": '2021',
+    "position": 'Position of work experience 1'
+  },
+  {
+    "description": 'Description of position 2',
+    "date": '2021',
+    "position": 'Position of work experience 2'
+  }
+];
+
+
 export const fakeWorkExperiences: WorkExperience[] = [
   {
-    "description": 'Description of work experience 1',
-    "date": '2021',
     "skills": 'lang',
     "place": 'Place of work experience 1',
-    "position": 'Position of work experience 1',
+    "positions": fakePositions,
     "company": 'Company of work experience 1',
     "companyUrl": 'https://company.com',
     "companyLogo": 'https://logo.com'
