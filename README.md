581ddded67e294a17cc94845aa32d9f23be6809f
Romain Frezier <romainfrezier@gmail.com>
2025-01-18 18:52:57 +0100

    chore(credits): Update credits date.

--- README.md
+++ README.md
@@ -141,4 +141,4 @@ This project is licensed under the GNU General Public License v3.0 - see the [LI
 
 ---
 
-© Romain Frezier - 2024
+© Romain Frezier - 2025
--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -11,4 +11,10 @@ import {Component} from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  public currentYear: number;
+
+  constructor() {
+    this.currentYear = new Date().getFullYear();
+  }
+}
